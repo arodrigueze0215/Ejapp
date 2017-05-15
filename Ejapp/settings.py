188bf86d3d1dfdf58c2c97c87d83c1be95f9a354
arodrigueze <andres.rodriguez0215@gmail.com>
2017-05-15 17:00:57 -0500

    add statics and template folders

--- Ejapp/settings.py
+++ Ejapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Ejapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = '/static/'
+STATIC_URL = '/s/'
