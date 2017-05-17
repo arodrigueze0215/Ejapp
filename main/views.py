00a87fe882101e0ab25d587769331fcc09a1f9b3
arodrigueze <andres.rodriguez0215@gmail.com>
2017-05-17 17:45:17 -0500

    add bootstrap to de first template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+def inscriptions_add(request):
+     return render(request, 'inscription.html')
