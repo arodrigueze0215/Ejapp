3a2b37aca7926119d0e7ace27a79c153ff689d25
arodrigueze <andres.rodriguez0215@gmail.com>
2017-05-22 19:57:36 -0500

    add fields father

--- static/js/inscription.js
+++ static/js/inscription.js
@@ -1,7 +1,29 @@
-$(function (){
-    $("#example-embed").steps({
-        headerTag: "h3",
-        bodyTag: "section",
-        transitionEffect: "fade"
-    });
+$(function (){ 
+
+   $('input[type=radio][name=study]').change(function(){
+       switch($(this).val()) {
+         case 'true':
+            $('#career').removeAttr('disabled');
+            $('#institute').removeAttr('disabled');
+             break;
+         case 'false':
+          $('#career').attr('disabled', 'disabled');
+          $('#institute').attr('disabled', 'disabled');
+             break;
+     }
+   });
+    $('input[type=radio][name=job]').change(function(){
+       switch($(this).val()) {
+         case 'true':
+          $('#company').removeAttr('disabled');         
+          $('#role').removeAttr('disabled');         
+          $('#phone-company').removeAttr('disabled');         
+             break;
+         case 'false':
+          $('#company').attr('disabled', 'disabled');         
+          $('#role').attr('disabled', 'disabled');         
+          $('#phone-company').attr('disabled', 'disabled');
+             break;
+     }
+   });
 })
