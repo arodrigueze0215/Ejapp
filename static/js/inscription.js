5bac4c5e81ece8439bd8a262d1e2e9120379caa8
arodrigueze <andres.rodriguez0215@gmail.com>
2017-05-26 17:14:36 -0500

    add form data mom and dad

--- static/js/inscription.js
+++ static/js/inscription.js
@@ -26,4 +26,40 @@ $(function (){
              break;
      }
    });
+   $('input[type=radio][name=dad]').change(function(){
+       switch($(this).val()) {
+         case 'true':
+          $('#name-dad').removeAttr('disabled', 'disabled');         
+          $('#ocupation-dad').removeAttr('disabled', 'disabled');         
+          $('#phone-dad').removeAttr('disabled', 'disabled');
+          $('#mobile-dad').removeAttr('disabled', 'disabled');
+          $('#address-dad').removeAttr('disabled', 'disabled');         
+             break;
+         case 'false':
+          $('#name-dad').attr('disabled', 'disabled');         
+          $('#ocupation-dad').attr('disabled', 'disabled');         
+          $('#phone-dad').attr('disabled', 'disabled');
+          $('#mobile-dad').attr('disabled', 'disabled');
+          $('#address-dad').attr('disabled', 'disabled');
+             break;
+     }
+   });
+    $('input[type=radio][name=mom]').change(function(){
+       switch($(this).val()) {
+         case 'true':
+          $('#name-mom').removeAttr('disabled', 'disabled');         
+          $('#ocupation-mom').removeAttr('disabled', 'disabled');         
+          $('#phone-mom').removeAttr('disabled', 'disabled');
+          $('#mobile-mom').removeAttr('disabled', 'disabled');
+          $('#address-mom').removeAttr('disabled', 'disabled');         
+             break;
+         case 'false':
+          $('#name-mom').attr('disabled', 'disabled');         
+          $('#ocupation-mom').attr('disabled', 'disabled');         
+          $('#phone-mom').attr('disabled', 'disabled');
+          $('#mobile-mom').attr('disabled', 'disabled');
+          $('#address-mom').attr('disabled', 'disabled');
+             break;
+     }
+   });
 })
