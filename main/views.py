839d725b7f10aa9f8b8f57a8247ab887046e1f74
Andres Rodriguez <andres.rodriguez0215@gmail.com>
2017-07-30 22:28:44 -0500

    add list fds template

--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.template import loader
 
-# Create your views here.
+
 def inscriptions_add(request):
      return render(request, 'inscrip.html')
+
+def list_fds(request):
+     return render(request, 'fds-list.html')
