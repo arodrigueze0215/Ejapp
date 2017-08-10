b3f5d9df067a6ea65b77df3c0ffe7d6eddf72fec
arodrigueze <andres.rodriguez0215@gmail.com>
2017-08-10 18:02:06 -0500

    merge

--- main/views.py
+++ main/views.py
@@ -6,9 +6,6 @@ from django.template import loader
 
 def inscriptions_add(request):
      return render(request, 'inscrip.html')
-<<<<<<< HEAD
-=======
 
 def list_fds(request):
      return render(request, 'fds-list.html')
->>>>>>> e338e6eb9500113d30a102a169078de321253f42
